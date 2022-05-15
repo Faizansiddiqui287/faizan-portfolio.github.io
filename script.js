71262f64708f1510d4b3e0840ff20f7097e41653
Faizan Siddiqui <91340388+Faizansiddiqui287@users.noreply.github.com>
2022-05-15 19:44:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ $('.menu-btn').click(function(){
     $('.navbar .menu').toggleClass("active");
     $('.menu-btn i').toggleClass("active");
 }); 
+
+let myFun = () => {
+    let name = document.getElementById('name1').value
+    let email = document.getElementById('email1').value
+    let subject = document.getElementById('subject1').value
+    let query = document.getElementById('describe1').value
+    alert("Name : " + name + "\n Email : " + email + "\n Subject : " + subject + "\n Query : " + query);
+}
